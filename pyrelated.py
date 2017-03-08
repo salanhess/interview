bea58d2f55280317bb9e99212a39ae492982a44b
salanhess <salanhess@126.com>
2017-03-09 00:30:26 +0800

    Update pyrelated.py
    |...>apps>Cloud Sy...>老王python...>基础篇5-pyt..

--- pyrelated.py
+++ pyrelated.py
@@ -1,5 +1,6 @@
 import string
 
+#<Old Wang Based Python>
 str1 = "  pythonisgreat  "
 #1.remove space in the left
 print(str1.lstrip())
@@ -16,3 +17,17 @@ print(str1.upper().strip())
 #5.lower chars
 str2="AbC"
 print(str2.lower())
+
+#6.check var type:str/list
+print(type(str2))
+print(type(str1.split()))
+
+#Chapter5
+info = 'abdd'
+#replace d with c,count=1 means ONLY replace once
+print(info.replace('d','c',1))
+
+#convert string to int then can add
+a = '1'
+b = 2
+print(int(a)+b)
