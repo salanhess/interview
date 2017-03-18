881acfbb556c7517be729fc2d2cf20b08121cde8
salanhess <salanhess@126.com>
2017-03-18 23:34:09 +0800

    add selenium

--- Selenium_study/C13_multiWindows_handles.py
+++ Selenium_study/C13_multiWindows_handles.py
@@ -1,4 +1,7 @@
 #coding=utf-8
+# refer to http://www.cnblogs.com/yoyoketang/p/6128611.html
+#todo loop with error :    raise exception_class(message, screen, stacktrace)
+#selenium.common.exceptions.StaleElementReferenceException: Message: The element reference is stale. Either the element is no longer attached to the DOM or the page has been refreshed.
 
 from selenium import webdriver
 import time
@@ -22,25 +25,22 @@ h = driver.current_window_handle
 # 定位网页、贴吧等链接
 s = driver.find_elements_by_xpath(".//*[@id='header-link-wrapper']/li")
 r = [u"网页", u"新闻",u"贴吧", u"知道",  u"音乐", u"图片", u"视频", u"地图", u"文库"]
-for a in s:
-    print(driver.title)
+
+for a, b in zip(s, r):
     a.click()
-    driver.switch_to.active_element
-    print(driver.title)
-    print(a.text)
+    #text = a.text
+    time.sleep(2)
     all_h = driver.window_handles
-    print(all_h)
-#
-# print(driver.title)
-# s[3].click()
-# time.sleep(2)
-# driver.switch_to.active_element
-# print(driver.title)
-#
-# for a in s:
-#     a.click()
-#     time.sleep(4)
-#
-#     text = a.text
-#     all_h = driver.window_handles
-#     print(all_h)
+    # 循环判断是否与首页句柄相等
+    for i in all_h:
+        if i != h:
+            driver.switch_to.window(i)
+            time.sleep(1)
+    print(driver.title)
+    if b in driver.title:
+        print(u"页面打开正常")
+    else:
+        print(u"页面测试失败")
+    driver.close()                       # 关闭当前页面
+    driver.switch_to.window(h)           # 切回句柄到首页
+driver.quit()
