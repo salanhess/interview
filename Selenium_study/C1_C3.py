08f709afb935b85d3802367bd5b0290e52911e2d
salanhess <salanhess@126.com>
2017-03-18 17:58:58 +0800

    add selenium

--- Selenium_study/C1_C3.py
+++ Selenium_study/C1_C3.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 import time
 
+'''selenium的webdriver提供了八种基本的元素定位方法，前面六种是通过元素的属性来直接定位的，后面的xpath和css定位更加灵活，需要重点掌握其中一个。
+1.通过id定位：find_element_by_id()
+2.通过name定位：find_element_by_name()
+3.通过class定位：find_element_by_class_name()
+4.通过tag定位：find_element_by_tag_name()
+5.通过link定位：find_element_by_link_text()
+6.通过partial_link定位：find_element_by_partial_link_text()
+7.通过xpath定位：find_element_by_xpath()
+8.通过css定位：find_element_by_css_selector()
+'''
 #Basic click/sed_keys operation: open baidu,search xxx,open the second searched result.
 browser = webdriver.Firefox()
 browser.get("https://www.baidu.com")
--- Selenium_study/C7_xpath_findmore.py
+++ Selenium_study/C7_xpath_findmore.py
@@ -6,7 +6,7 @@ import time
 #
 browser = webdriver.Firefox()
 browser.get("https://www.baidu.com")
-print(browser.find_element_by_xpath("//*[contains(text(),'hao123')]"))
-print(browser.find_elements_by_xpath("//*[@class='mnav' and @href='hao123']"))
+browser.find_elements_by_xpath("//*[@class='mnav' and @href='http://www.hao123.com']")[0].click()
+
 time.sleep(5)
 browser.quit()
