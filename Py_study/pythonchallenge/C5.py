d29ae94ff0686088d0e19a25a9fa0666311a05dd
salanhess <salanhess@126.com>
2017-04-23 10:32:07 +0800

    block at C5: need use pickle to dump

--- Py_study/pythonchallenge/C5.py
+++ Py_study/pythonchallenge/C5.py
@@ -1,4 +1,17 @@
-#coding=utf-8
+#coding = utf-8
 
 #http://www.pythonchallenge.com/pc/def/peak.html
+
+import pickle
+
+
+pkl_file = open('C5_banner.p', 'rb')
+selfref_list= pickle.load(pkl_file)
+
+with open('C5_info.txt','wb') as f:
+    pickle.dump(selfref_list,f)
+
+with open('C5_info.txt') as f:
+    print(pickle.load(f).encode('utf-8'))
+
 
