eed69bb65a7bd4fa5acfea3764e8e15fb8d9c254
salanhess <salanhess@126.com>
2017-05-25 18:58:03 +0800

    Update C3_threadpooling.py

--- Py_study/TaskInvest/C3_threadpooling.py
+++ Py_study/TaskInvest/C3_threadpooling.py
@@ -1,1 +1,4 @@
 #refer to https://www.ibm.com/developerworks/cn/aix/library/au-threadingpython/?ca=drs-tp3008
+http://www.dongwm.com/archives/%E4%BD%BF%E7%94%A8Python%E8%BF%9B%E8%A1%8C%E5%B9%B6%E5%8F%91%E7%BC%96%E7%A8%8B-%E7%BA%BF%E7%A8%8B%E7%AF%87/
+  
+  
